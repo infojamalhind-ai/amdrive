c33a758986bea18a4825439343cf0bc044227166
Mohammed Ayub <infoameerarentcar@gmail.com>
2026-04-23 19:50:57 +0400

    Add Microsoft Clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { getSiteUrl } from "@/lib/site-url";
 import "./globals.css";
 
@@ -50,7 +51,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script id="microsoft-clarity-analytics" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "v6vzu7zpni");`}
+        </Script>
+      </body>
     </html>
   );
 }
